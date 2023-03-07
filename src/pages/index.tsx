cef4182f12e4a569f81bad61d88bee62a9e4618c
Daniel Truong <aduy1122@gmail.com>
2023-03-06 17:08:07 -0800

    Staging branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import LandingPage from '@/components/LandingPage/LandingPage'
-
+//This is staging branch
 export default function Home() {
   return (
     <main className="relative flex flex-col items-center font-rubik font-semibold">
