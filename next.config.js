1d80595d2291039cf834bb0cb712ef530b11b01f
Daniel Truong <aduy1122@gmail.com>
2023-03-07 21:01:44 -0800

    added nextauthurl

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXTAUTH_URL: 'https://fullyhacks.netlify.app/'
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
