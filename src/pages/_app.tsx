cd03738c5d17efade6bbe3ad70375d578c18f2ca
Daniel Truong <aduy1122@gmail.com>
2023-03-07 21:18:16 -0800

    Netlify NextAuth works pls

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Session } from 'next-auth'
 import { SessionProvider as AuthProvider } from 'next-auth/react'
 
 export default function App({
   Component,
-  pageProps: { session, ...pageProps }
-}: AppProps) {
+  pageProps
+}: AppProps<{ session: Session }>) {
   return (
     <>
-      <AuthProvider session={session}>
+      <AuthProvider session={pageProps.session}>
         <section className="scroll-smooth bg-body_bg w-[100vw] h-[100vh] flex flex-col overflow-scroll overflow-x-hidden">
           <Component {...pageProps} />
         </section>
