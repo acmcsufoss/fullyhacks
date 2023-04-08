d0727a9b4cdf4dfaf91154f208313a91d8041ef3
Daniel Truong <aduy1122@gmail.com>
2023-04-08 01:43:23 -0700

    added link

--- next.config.js
+++ next.config.js
@@ -20,6 +20,11 @@ const nextConfig = {
         permanent: true
       },
       {
+        source: '/projectform',
+        destination: 'https://forms.gle/s6qTwJbf7TDHs3dT9',
+        permanent: true
+      },
+      {
         source: '/opening',
         destination:
           'https://www.canva.com/design/DAFe5xstJ38/mJqwCHRVGnmUnHcGI7GbyA/view?utm_content=DAFe5xstJ38&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink',
@@ -27,12 +32,14 @@ const nextConfig = {
       },
       {
         source: '/nextjs',
-        destination: 'https://www.canva.com/design/DAFfUTCnxRM/dOmLiwmTcAK6OZAuy84QZw/view?utm_content=DAFfUTCnxRM&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink',
+        destination:
+          'https://www.canva.com/design/DAFfUTCnxRM/dOmLiwmTcAK6OZAuy84QZw/view?utm_content=DAFfUTCnxRM&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink',
         permanent: true
       },
       {
         source: '/mobiledev',
-        destination: 'https://docs.google.com/presentation/d/14KNA1VjsvGRGFQsj8Yi7p0aFqW-nejssvCfwXHkbPYE/edit?usp=sharing',
+        destination:
+          'https://docs.google.com/presentation/d/14KNA1VjsvGRGFQsj8Yi7p0aFqW-nejssvCfwXHkbPYE/edit?usp=sharing',
         permanent: true
       }
     ]
