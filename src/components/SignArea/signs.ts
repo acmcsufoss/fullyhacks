7a667072eb0f670dc536534a4e9e8de5d8d635ea
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-28 23:33:39 -0800

    add more signs to left-side

--- src/components/SignArea/signs.ts
+++ src/components/SignArea/signs.ts
@@ -5,15 +5,50 @@ export const SIGNS: SignProps[] = [
     textContent: 'Welcome',
     borderColor: '#4F96B9',
     parallaxSpeed: 50,
-    top: '24vh',
+    top: '15vh',
     left: '2vw'
   },
   {
-    textContent: '完全に',
+    textContent: '優待',
     borderColor: '#932B8E',
     parallaxSpeed: 60,
-    top: '40vh',
-    right: '10vw'
+    top: '18vh',
+    left: '8vw'
+  },
+  {
+    textContent: 'Hacks',
+    borderColor: '#9581AA',
+    parallaxSpeed: 55,
+    top: '26vh',
+    left: '9vw'
+  },
+  {
+    textContent: 'たたき切る',
+    borderColor: '#7A9FFF',
+    parallaxSpeed: 20,
+    top: '55vh',
+    left: '3vw'
+  },
+  {
+    textContent: '優待',
+    borderColor: '#9581AA',
+    parallaxSpeed: 30,
+    top: '57vh',
+    left: '11vw'
+  },
+  {
+    textContent: 'Fully',
+    borderColor: '#6CCAFF',
+    parallaxSpeed: 42,
+    top: '65vh',
+    left: '7vw'
+  },
+  {
+    textContent: 'ACM',
+    borderColor: '#FC61FF',
+    parallaxSpeed: 54,
+    top: '61vh',
+    left: '-2vw'
   }
   // TODO: Add more signs according to the Figma.
 ]
