22eb737ce330c683c336489bbd20810f637670df
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-28 23:08:27 -0800

    decrease opacity with scroll

--- src/components/SignArea/signs.ts
+++ src/components/SignArea/signs.ts
@@ -4,14 +4,14 @@ export const SIGNS: SignProps[] = [
   {
     textContent: 'Welcome',
     borderColor: '#4F96B9',
-    parallaxSpeed: 10,
+    parallaxSpeed: 50,
     top: '24vh',
     left: '2vw'
   },
   {
     textContent: '完全に',
     borderColor: '#932B8E',
-    parallaxSpeed: 20,
+    parallaxSpeed: 60,
     top: '40vh',
     right: '10vw'
   }
