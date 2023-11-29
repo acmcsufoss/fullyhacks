036a844ace23ba25cfb8107555719593230a42bb
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-28 22:31:59 -0800

    add temporary polyfill for `animation-timeline`
    Reference: <https://github.com/flackr/scroll-timeline#readme>.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* https://github.com/flackr/scroll-timeline#readme */}
+        <Script
+          strategy="beforeInteractive"
+          src="https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js"
+        />
       </body>
     </Html>
   )
