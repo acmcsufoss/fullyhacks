14c64bf635a6912e1ed09a2d8757b252d37e9146
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-12-04 13:09:24 -0800

    Add neon sign components (#33)
    * add neon sign components
    
    ### Reference
    
    - <https://youtu.be/Qj0Qx8HpNUo> ([code](https://github.com/kevin-powell/css-parallax))
    
    * delete unused commented code
    
    * add boldness
    
    * use text stroke for inner sign text
    
    * move inline styles to globals.css
    
    * add temporary polyfill for `animation-timeline`
    
    Reference: <https://github.com/flackr/scroll-timeline#readme>.
    
    * decrease opacity with scroll
    
    * add more signs to left-side
    
    * mirror signs on left-side
    
    * increase sign element opacity transition on hover
    
    * add sign flickering animation
    
    Additionally adds JSDoc comments to properties of `SignProps` type.
    
    * update flags on right side to better match designs
    
    * wip
    
    * wip
    
    * force page reload when user navigates to `/`
    
    Pro: fixes the weird Firefox bug.
    Con: navigating back to home page is slow.
    
    * rip out force refresh code
    
    CEO Danny says it is time to let the force-refresh code go. I agree with him.
    
    Context:
    > [11:28 PM]dani: maybe in the LandingPage.jsx, we can add a useEffect() with router.refresh() right there
    > [11:29 PM]dani: wait nvm
    > [11:29 PM]dani: that'll be an inf loop of refresh 💀
    > [11:31 PM]dani: or tbh we can just leave it like before, not worth to force refresh the page

---
+++ src/components/SignArea/signs.ts
@@ -0,0 +1,124 @@
+import type { SignProps } from './Sign'
+
+export const SIGNS: SignProps[] = [
+  // Left side.
+  {
+    textContent: 'Welcome',
+    borderColor: '#4F96B9',
+    parallaxSpeed: 50,
+    top: '15vh',
+    left: '2vw'
+  },
+  {
+    textContent: '優待',
+    borderColor: '#932B8E',
+    parallaxSpeed: 60,
+    flickerDelay: 0.5,
+    top: '18vh',
+    left: '8vw'
+  },
+  {
+    textContent: 'Hacks',
+    borderColor: '#9581AA',
+    parallaxSpeed: 55,
+    flickerDelay: 1,
+    top: '26vh',
+    left: '9vw'
+  },
+  {
+    textContent: 'たたき切る',
+    borderColor: '#7A9FFF',
+    parallaxSpeed: 20,
+    flickerDelay: 1.5,
+    top: '50vh',
+    left: '3vw'
+  },
+  {
+    textContent: '優待',
+    borderColor: '#9581AA',
+    parallaxSpeed: 30,
+    flickerDelay: 2,
+    top: '52vh',
+    left: '11vw'
+  },
+  {
+    textContent: 'Fully',
+    borderColor: '#6CCAFF',
+    parallaxSpeed: 42,
+    flickerDelay: 2.5,
+    top: '60vh',
+    left: '7vw'
+  },
+  {
+    textContent: 'ACM',
+    borderColor: '#FC61FF',
+    parallaxSpeed: 54,
+    flickerDelay: 3,
+    top: '56vh',
+    left: '-2vw'
+  },
+  // Right side.
+  {
+    textContent: 'ACM',
+    borderColor: '#FC61FF',
+    parallaxSpeed: 50,
+    top: '15vh',
+    right: '2vw'
+  },
+  {
+    textContent: 'たたき切る',
+    borderColor: '#9EAEFF',
+    parallaxSpeed: 60,
+    flickerDelay: 0.5,
+    top: '18vh',
+    right: '8vw'
+  },
+  {
+    textContent: 'Fully',
+    borderColor: '#6CCAFF',
+    parallaxSpeed: 55,
+    flickerDelay: 1,
+    top: '36vh',
+    right: '9vw'
+  },
+  {
+    textContent: 'Hacks',
+    borderColor: '#9581AA',
+    parallaxSpeed: 20,
+    flickerDelay: 1.5,
+    top: '39vh',
+    right: '3vw'
+  },
+  {
+    textContent: 'ACM',
+    borderColor: '#FC61FF',
+    parallaxSpeed: 30,
+    flickerDelay: 2,
+    top: '57vh',
+    right: '5vw'
+  },
+  {
+    textContent: '優待',
+    borderColor: '#9EAEFF',
+    parallaxSpeed: 22,
+    flickerDelay: 0.5,
+    top: '65vh',
+    right: '12vw'
+  },
+  {
+    textContent: 'Fully',
+    borderColor: '#6CCAFF',
+    parallaxSpeed: 42,
+    flickerDelay: 2.5,
+    top: '70vh',
+    right: '7vw'
+  },
+  {
+    textContent: 'ACM',
+    borderColor: '#9EAEFF',
+    parallaxSpeed: 54,
+    flickerDelay: 3,
+    top: '72vh',
+    right: '2vw'
+  }
+]
