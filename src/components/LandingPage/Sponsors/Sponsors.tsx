6e2635704e930fc25310b79c2137bbe84661a234
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-12-29 16:01:34 -0800

    Started implementing Sponsors component

--- src/components/LandingPage/Sponsors/Sponsors.tsx
+++ src/components/LandingPage/Sponsors/Sponsors.tsx
@@ -1,6 +1,28 @@
 import { companyType } from '@/types/interface'
 import React from 'react'
 
+interface SponsorBalloonProps {
+  company: companyType
+}
+
+const SponsorBalloon: React.FC<SponsorBalloonProps> = (props) => {
+  const { company } = props
+
+  return (
+    <div className="relative">
+      <div className="flex items-center mt-4 md:mt-8 md:mr-24" key={company.id}>
+        <a target="_blank" href={company.href}>
+          <img
+            src={company.image}
+            alt={company.name}
+            className="object-cover relative z-20 w-32 md:w-52"
+          />
+        </a>
+      </div>
+    </div>
+  )
+}
+
 interface SponsorProps {
   companies: companyType[]
 }
@@ -8,26 +30,13 @@ interface SponsorProps {
 const Sponsors: React.FC<SponsorProps> = (props) => {
   const { companies } = props
   return (
-    <div className="md:ml-[6rem]">
-      <p className="mt-10 text-xl font-medium md:text-xxl">Sponsors</p>
-      <p className="mt-4 font-light">
-        Organizations that help make FullyHacks 2023 possible
-      </p>
-      <section className="grid grid-cols-3 items-center justify-center gap-8 my-10 ">
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="mt-10 text-center text-xxl text-[#D7EEFF] [text-shadow:_0_0_10px_#719BCC] font-medium font-ohm md:text-[5rem]">
+        Sponsors
+      </h2>
+      <section className="grid grid-cols-3 items-center justify-center gap-8 my-10">
         {companies.map((company: companyType) => {
-          return (
-            <div
-              className="flex items-center mt-4 md:mt-8 md:mr-24"
-              key={company.id}>
-              <a target="_blank" href={company.href}>
-                <img
-                  src={company.image}
-                  alt={company.name}
-                  className="object-cover relative z-20 w-32 md:w-52"
-                />
-              </a>
-            </div>
-          )
+          return <SponsorBalloon key={company.id} company={company} />
         })}
       </section>
     </div>
--- src/pages/api/companies/companies.ts
+++ src/pages/api/companies/companies.ts
@@ -15,50 +15,20 @@ export const companies: companyType[] = [
   },
   {
     id: 'co3',
+    name: 'Google',
+    href: 'http://www.google.com/',
+    image: '/google.png'
+  },
+  {
+    id: 'co4',
     name: 'Wolfram',
     href: 'https://www.wolfram.com/language/',
     image: '/wolfram.png'
   },
   {
-    id: 'co4',
-    name: 'XYZ',
-    href: 'https://gen.xyz/',
-    image: '/xyz.png'
-  },
-  {
-    id: 'co6',
-    name: 'StickerMule',
-    href: 'https://mule.to/p3vq',
-    image: '/stickermule.png'
-  },
-  {
     id: 'co5',
-    name: 'Balsamiq',
-    href: 'https://balsamiq.com/',
-    image: '/balsamiq.png'
-  },
-  {
-    id: 'co7',
-    name: 'Prisma',
-    href: 'https://www.prisma.io/',
-    image: '/prisma.png'
-  },
-  {
-    id: 'co8',
-    name: 'echo3D',
-    href: 'https://www.echo3d.com/',
-    image: '/echo3d.png'
-  },
-  {
-    id: 'co9',
-    name: 'Celsius',
-    href: 'https://www.celsius.com/',
-    image: '/celsius.png'
-  },
-  {
-    id: 'co10',
-    name: 'Digital Ocean',
-    href: 'https://www.digitalocean.com/',
-    image: '/digitalocean.png'
+    name: 'StickerGiant',
+    href: 'https://www.stickergiant.com/',
+    image: '/stickergiant.png'
   }
 ]
