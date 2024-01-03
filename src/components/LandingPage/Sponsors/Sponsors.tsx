a68803380a27bf8e1ced601b8ca3f16d94a18dec
Tomas Oh <112128328+tomasohCHOM@users.noreply.github.com>
2024-01-03 13:11:51 -0800

    Implemented Sponsors component (#44)
    * Started implementing Sponsors component
    
    * Added Sponsor Balloons

--- src/components/LandingPage/Sponsors/Sponsors.tsx
+++ src/components/LandingPage/Sponsors/Sponsors.tsx
@@ -1,6 +1,27 @@
 import { companyType } from '@/types/interface'
 import React from 'react'
 
+interface SponsorBalloonProps {
+  company: companyType
+}
+
+const SponsorBalloon: React.FC<SponsorBalloonProps> = (props) => {
+  const { company } = props
+
+  return (
+    <div className="w-52 md:w-72 relative motion-reduce:animate-none animate-float">
+      <img src="/sponsor_balloon.svg" alt="Sponsor Balloon" />
+      <a className="text-center" target="_blank" href={company.href}>
+        <img
+          className="absolute z-20 w-32 md:w-48 top-0 bottom-14 md:bottom-20 left-0 right-0 m-auto"
+          src={company.image}
+          alt={company.name}
+        />
+      </a>
+    </div>
+  )
+}
+
 interface SponsorProps {
   companies: companyType[]
 }
@@ -8,26 +29,13 @@ interface SponsorProps {
 const Sponsors: React.FC<SponsorProps> = (props) => {
   const { companies } = props
   return (
-    <div className="md:ml-[6rem]">
-      <p className="mt-10 text-xl font-medium md:text-xxl">Sponsors</p>
-      <p className="mt-4 font-light">
-        Organizations that help make FullyHacks 2023 possible
-      </p>
-      <section className="grid grid-cols-3 items-center justify-center gap-8 my-10 ">
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="mt-10 text-center text-xxl text-[#D7EEFF] [text-shadow:_0_0_10px_#719BCC] font-medium font-ohm md:text-[5rem]">
+        Sponsors
+      </h2>
+      <section className="grid grid-cols-2 lg:grid-cols-3 items-center justify-center gap-4 md:gap-x-12 my-10">
         {companies.map((company: companyType) => {
-          return (
-            <div
-              className="flex items-center mt-4 md:mt-8 md:mr-24"
-              key={company.id}>
-              <a target="_blank" href={company.href}>
-                <img
-                  src={company.image}
-                  alt={company.name}
-                  className="object-cover relative z-20 w-32 md:w-52"
-                />
-              </a>
-            </div>
-          )
+          return <SponsorBalloon key={company.id} company={company} />
         })}
       </section>
     </div>
