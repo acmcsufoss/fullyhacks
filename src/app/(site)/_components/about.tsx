e6832196d72fb69c553b5bdc9646f5ad2965adc7
Gustavo Necochea <necocheagustavo@gmail.com>
2025-01-05 12:10:49 -0800

    Updated about.tsx for large screens

--- src/app/(site)/_components/about.tsx
+++ src/app/(site)/_components/about.tsx
@@ -10,9 +10,9 @@ export default function About() {
       <div
         id="about"
         className="absolute left-1/2 top-[53%] z-10 h-[50%] w-[90%] -translate-x-1/2 -translate-y-1/2 transform text-black sm:top-[62%] sm:w-[75%]">
-        <h2 className="text-lg font-bold sm:text-xl">About</h2>
+        <h2 className="text-lg font-bold sm:text-xl md:text-xxl">About</h2>
 
-        <p className="rounded-2xl bg-[#7F7F7F] p-2 pb-6 text-xs font-semibold sm:mt-2 sm:text-[1rem] sm:leading-relaxed">
+        <p className="rounded-2xl bg-[#7F7F7F] p-2 pb-6 text-xs font-semibold sm:mt-2 sm:text-[1rem] sm:leading-relaxed md:text-lg">
           We are Cal State University Fullerton&apos;s (CSUF) student-run
           hackathon.
           <br />
