30f2371af1b6932ac4e6dba52fcc1ddaf9ef0923
Gustavo Necochea <necocheagustavo@gmail.com>
2025-01-05 11:54:50 -0800

    Update about.tsx for responsive design

--- src/app/(site)/_components/about.tsx
+++ src/app/(site)/_components/about.tsx
@@ -3,17 +3,16 @@ import React from "react";
 export default function About() {
   return (
     <section className="relative z-0 font-audiowide">
-      {/* about image */}
+      {/* Moon Image */}
       <img src="assets/about.png" alt="moon" />
 
-      {/* text */}
+      {/* Text Content */}
       <div
         id="about"
-        className="absolute left-1/2 top-[55%] z-10 w-[75%] -translate-x-1/2 -translate-y-1/2 transform p-4 text-black md:p-12">
-        {/* about heading */}
-        <h2 className="text-9xl font-semibold md:text-5xl">About</h2>
-        {/* about body */}
-        <p className="rounded-2xl bg-[#7F7F7F] p-2 pb-6 text-2xl font-semibold md:mt-4">
+        className="absolute left-1/2 top-[53%] z-10 h-[50%] w-[90%] -translate-x-1/2 -translate-y-1/2 transform text-black sm:top-[62%] sm:w-[75%]">
+        <h2 className="text-lg font-bold sm:text-xl">About</h2>
+
+        <p className="rounded-2xl bg-[#7F7F7F] p-2 pb-6 text-xs font-semibold sm:mt-2 sm:text-[1rem] sm:leading-relaxed">
           We are Cal State University Fullerton&apos;s (CSUF) student-run
           hackathon.
           <br />
@@ -28,4 +27,3 @@ export default function About() {
     </section>
   );
 }
-2;
