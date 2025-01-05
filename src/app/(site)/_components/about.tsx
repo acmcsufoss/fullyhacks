4fafa85bd434c14a4ecdcdbbfa5c0dc7f5e0adec
Gustavo Necochea <necocheagustavo@gmail.com>
2025-01-04 19:49:29 -0800

    Add about iamge and update about.tsx

--- src/app/(site)/_components/about.tsx
+++ src/app/(site)/_components/about.tsx
@@ -2,37 +2,30 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className="relative flex w-[min(40rem,_80%)] flex-col rounded-2xl border-4 border-pink_200 bg-[#1E0072] bg-opacity-20">
-      <div id="about" className="relative p-4 text-[#EFE2FF] md:p-12">
-        <h2 className="mt-20 text-xl font-semibold text-[#8423FF] md:text-5xl">
-          About
-        </h2>
-        <p className="md:mt-4">
-          <span className="font-semibold text-[#00ABF5]">
-            24-hour hackathon{" "}
-          </span>
-          for beginners and first-time hackers. This year, our hackathon will be
-          held on
-          <span className="font-semibold text-[#385DDF]">
-            {" "}
-            February 24th to 25th
-          </span>
-          .
-        </p>
-        <p className="mt-4 md:mt-8">
-          You can even
-          <span className="font-semibold text-[#4F38DF]">
-            {" "}
-            meet representatives{" "}
-          </span>
-          from our generous sponsors during our networking event. Your project
-          also has the chance to
-          <span className="font-semibold text-pink_700">
-            {" "}
-            win cool prizes! 🎉
-          </span>
+    <section className="relative z-0 font-audiowide">
+      {/* about image */}
+      <img src="assets/about.png" alt="moon" />
+
+      {/* text */}
+      <div
+        id="about"
+        className="absolute left-1/2 top-[55%] z-10 w-[75%] -translate-x-1/2 -translate-y-1/2 transform p-4 text-black md:p-12">
+        {/* about heading */}
+        <h2 className="text-9xl font-semibold md:text-5xl">About</h2>
+        {/* about body */}
+        <p className="rounded-2xl bg-[#7F7F7F] p-2 pb-6 text-2xl font-semibold md:mt-4">
+          We are Cal State University Fullerton&apos;s (CSUF) student-run
+          hackathon.
+          <br />
+          We plan to host 600 students for a 24-hour coding event in April 2025.
+          <br />
+          <br />
+          Our goal is to bring together students interested in building
+          impactful, innovative projects, with the bonus opportunity to network
+          with industry professionals and win prizes!
         </p>
       </div>
     </section>
   );
 }
+2;
