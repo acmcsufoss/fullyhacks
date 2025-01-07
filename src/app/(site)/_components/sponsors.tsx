7834ceac69c2200055f4d2290ce3f66eea834236
Jeremiah <jeremherring@gmail.com>
2025-01-06 16:07:18 -0800

    make position changes

--- src/app/(site)/_components/sponsors.tsx
+++ src/app/(site)/_components/sponsors.tsx
@@ -22,42 +22,46 @@ interface SponsorProps {
 const Sponsors: React.FC<SponsorProps> = (props) => {
   const { companies } = props;
 
-  // Predefined positions for larger screens
   const zones = [
-    { top: "20%", left: "40%" },
-    { top: "35%", left: "20%" },
-    { top: "50%", left: "50%" },
+    { top: "30%", left: "40%" },
+    { top: "45%", left: "20%" },
+    { top: "55%", left: "50%" },
     { top: "60%", left: "10%" },
     { top: "70%", left: "30%" },
-    { top: "25%", left: "70%" },
   ];
 
   return (
-    <div className="relative h-[90vh] w-full overflow-hidden">
-      {/* UFO and Light Cone (Visible Only on Larger Screens) */}
+    <div className="min-h-screen w-full overflow-hidden">
       <div className="hidden md:block absolute top-0 left-0">
         <div className="relative">
-          {/* UFO */}
           <img
             src="/assets/combined_ufo.png"
             alt="UFO"
             className="w-[40vw] md:w-[30vw] lg:w-[25vw]"
           />
-          {/* Cone */}
           <img
             src="/assets/cone.svg"
             alt="Light Cone"
-            className="absolute top-[66%] left-[80%] transform -translate-x-1/2 w-[50vw] md:w-[40vw] lg:w-[35vw] opacity-70 animate-flicker"
+            style={{
+              width: "200vw",
+              height: "auto"
+            }}
+            className="absolute top-[66%] left-[80%] transform -translate-x-1/2 w-[150vw] opacity-70 animate-flicker"
           />
         </div>
       </div>
+      <div className="absolute top-[40%] left-[12%] transform -translate-x-1/2 -translate-y-1/2">
+      <img 
+            src="/assets/smaller_ufo.svg"
+            alt="ufo"
+            className="w-[30vw] md:w-[25vw] lg:w-[22vw]"
+            />
+        </div>
 
-      {/* Sponsors Section */}
       <div className="relative h-full w-full">
-        {/* Larger Screens: Random Asteroid Placement */}
         <div className="hidden md:block">
           {companies.map((company: companyType, index) => {
-            const position = zones[index % zones.length]; // Cycle through zones
+            const position = zones[index % zones.length]; 
 
             return (
               <div
@@ -75,7 +79,6 @@ const Sponsors: React.FC<SponsorProps> = (props) => {
           })}
         </div>
 
-        {/* Smaller Screens: Stacked Asteroids */}
         <div className="block md:hidden">
           <div className="flex flex-col items-center gap-6">
             {companies.map((company: companyType) => (
