f5dad221708f9cb8101c8b8767b6b88c7feea2f0
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-01-07 18:32:53 -0800

    update apply-auth for 2025

--- src/app/(site)/apply/_components/apply-auth.tsx
+++ src/app/(site)/apply/_components/apply-auth.tsx
@@ -1,21 +1,22 @@
 "use client";
 
 import React, { useState } from "react";
+
 interface ApplyAuthProps {
   children?: React.ReactNode;
 }
+
 const ApplyAuth: React.FC<ApplyAuthProps> = ({ children }) => {
-  // Application from March 13rd to April 1st
+  // 2025 application opens from Feb 14th to Mar 24th
+  // App extension ends on Mar 31st
+  const openDateTs = "2025-02-14T00:00:00.000-07:00";
+  const closeDateTs = "2025-03-24T23:59:59.999-07:00";
   const options = { timeZone: "America/Los_Angeles" };
-  const openDate = new Date("2024-01-14T00:00:00.000-07:00").toLocaleString(
-    "en-US",
-    options
-  );
-  const closeDate = new Date("2024-02-13T23:59:59.999-07:00").toLocaleString(
-    "en-US",
-    options
-  );
+
+  const openDate = new Date(openDateTs).toLocaleString("en-US", options);
+  const closeDate = new Date(closeDateTs).toLocaleString("en-US", options);
   const now = new Date().toLocaleString("en-US", options);
+
   const [applicationOpen, setOpen] = useState<boolean>(() => {
     const currentDate = new Date();
     const opening = new Date(openDate);
@@ -32,17 +33,21 @@ const ApplyAuth: React.FC<ApplyAuthProps> = ({ children }) => {
           {new Date(now) < new Date(openDate) && (
             <div className="flex flex-col items-center gap-12 text-center">
               <p className="w-[80%]">
-                Application will open on January 12th, see you soon!.
+                Application will open on Feb 14th, see you soon!.
               </p>
-              <img
-                src="/assets/byecat.svg"
-                alt="bye cat"
-                className="w-[300px]"
-              />
+              {/* TODO: update image here? */}
+              {/* <img */}
+              {/*   src="/assets/byecat.svg" */}
+              {/*   alt="bye cat" */}
+              {/*   className="w-[300px]" */}
+              {/* /> */}
             </div>
           )}
           {new Date(now) >= new Date(closeDate) && (
-            <p>Fullyhacks 2024 has ended, we&apos;ll see you next year 🐘.</p>
+            <p>
+              Fullyhacks {new Date(closeDate).getFullYear()} has ended,
+              we&apos;ll see you next year 🐘.
+            </p>
           )}
         </div>
       )}
