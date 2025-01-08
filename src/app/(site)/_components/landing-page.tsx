c8061dc2719930e51b7d7c20a46a797f67d49290
AndresArch <aloera23@student.rccd.edu>
2025-01-07 22:38:37 -0800

    completed the footer componet. I was able to make it responsive for small devices, medium, and large.

--- src/app/(site)/_components/landing-page.tsx
+++ src/app/(site)/_components/landing-page.tsx
@@ -58,12 +58,97 @@ const LandingPage: React.FC<LandingPageProps> = (props) => {
           <Team team={teamData} />
         </div>
       </section>
-      <section className="relative flex w-[100vw] flex-col items-center justify-start overflow-hidden pb-40 font-normal md:pt-32 md:pb-24 md:text-md">
+      <section className="relative flex w-[100vw] flex-col items-center justify-start overflow-hidden pb-6 font-normal md:pt-32 md:pb-6 md:text-md">
         <div
           id="frequently-asked-questions"
           className="relative mx-4 flex max-w-[1048px] flex-col items-center justify-center font-normal md:mt-40 md:text-md">
           <FAQ faqs={faqData} />
         </div>
+        {/* footer */}
+        {/* Stars footer*/}
+        <img
+          src="/assets/footer/stars.svg"
+          alt="stars"
+          className="absolute hidden lg:left-[48%] lg:bottom-0 lg:block lg:h-[73rem] lg:w-full lg:-translate-x-1/2 lg:animate-pulse"
+        />
+
+        {/* comets */}
+        <img
+          src="/assets/footer/shootingStars.svg"
+          alt="shooting stars"
+          className="absolute hidden w-full lg:bottom-[5rem] lg:block"
+        />
+
+        {/* capystroid */}
+        <img
+          src="/assets/footer/capyAstroid.svg"
+          alt="astroid capybara"
+          className="absolute bottom-[25rem] right-[4rem] h-[8rem] md:right-[7rem] md:h-[9rem] lg:bottom-[19rem] lg:right-[21rem] lg:h-[13rem] lg:w-auto"
+        />
+
+        {/* Satellite */}
+        <img
+          src="/assets/footer/satellite.svg"
+          alt="satellite"
+          className="absolute bottom-[32rem] right-[-1rem] h-[18rem] md:h-[20rem] lg:bottom-[25rem] lg:h-[34rem]"
+        />
+
+        {/* constellation 1 */}
+        <img
+          src="/assets/constellation.svg"
+          alt="constellation"
+          className="absolute hidden lg:bottom-[0rem] lg:left-[-20rem] lg:block lg:h-[65rem] lg:blur-md"
+        />
+
+        {/* Constellation 2 */}
+        <img
+          src="/assets/constellation.svg"
+          alt="constellation"
+          className="absolute hidden lg:bottom-[-35rem] lg:right-[-28rem] lg:block lg:h-[53rem] lg:rotate-[75deg] lg:blur-md"
+        />
+
+        {/* cloud haze */}
+        <img
+          src="/assets/footer/cloudy.svg"
+          alt="background"
+          className="absolute hidden lg:bottom-[-30rem] lg:block lg:h-[rem] lg:w-full"
+        />
+
+        {/* rocketboost */}
+        <img
+          src="/assets/footer/rocketBoost.svg"
+          alt="rocketboost"
+          className="absolute hidden lg:bottom-[27rem] lg:left-[2.5rem] lg:block lg:h-[14rem] lg:w-auto"
+        />
+
+        {/* Capybara on Spaceship */}
+        <img
+          src="/assets/footer/capyShip.svg"
+          alt="Spaceship"
+          className="absolute bottom-[36rem] left-[-2.5rem] h-[8.5rem] w-auto md:h-[10rem] lg:left-[3rem] lg:bottom-[30rem] lg:h-[22rem]"
+        />
+
+        {/* mobile bg */}
+        {/* constellation  */}
+        <img
+          src="/assets/footer/mobile/constellation.svg"
+          alt="constellations"
+          className="absolute bottom-[4rem] right-1 blur-lg md:right-[1rem] md:bottom-[-5rem] md:h-[70rem] lg:hidden"
+        />
+
+        {/* constellations multiple */}
+        <img
+          src="/assets/footer/mobile/constellationGroup.svg"
+          alt="constellations"
+          className="absolute bottom-[23rem] left-[0.25rem] h-[30rem] blur-lg md:bottom-[15rem] md:h-[40rem] lg:hidden"
+        />
+
+        {/* stars */}
+        <img
+          src="/assets/footer/mobile/mobileStars.svg"
+          alt="constellations"
+          className="absolute bottom-[20rem] left-[-2rem] h-[35rem] w-full animate-pulse lg:hidden md:h-[50rem] md:bottom-[9rem] md:left-[-9rem]"
+        />
         <LandingFooter />
       </section>
     </main>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,36 +1,65 @@
 import React from "react";
-import { BsInstagram, BsLinkedin, BsDiscord } from "react-icons/bs";
+import { BsLinkedin, BsDiscord } from "react-icons/bs";
+import { PiInstagramLogoFill } from "react-icons/pi";
 
 export const LandingFooter: React.FC = () => {
   return (
     <>
-      <div className="relative flex pt-80">
-        <div className="flex w-full flex-col items-center">
-          <h2 className="m-4 mt-14 text-center text-xxl font-medium text-[#B479FF] [text-shadow:_0_0_10px_#FFD8FD] md:text-[5rem]">
-            Contact Us
-          </h2>
-          <p className="font-medium">fullyhacks@gmail.com</p>
-          <div className="mt-4 flex items-center justify-center gap-4 text-[#9A76FF]">
+      <div className="relative mt-[25rem] flex pt-80 lg:mt-[50rem]">
+        <div className="flex w-full flex-grow flex-col items-center">
+          {/* Contact Us */}
+          <div className="relative inline-block">
+            <h2 className="m-4 mt-14 text-center font-audiowide text-[2rem] font-normal text-[#ECECEC] md:text-[3rem] lg:block lg:text-[5rem]">
+              CONTACT US
+            </h2>
+            {/* capy on the C */}
+            <img
+              src="/assets/footer/capyC.svg"
+              alt="Capybara"
+              className="absolute hidden w-auto sm:hidden lg:top-[1.65rem] lg:left-[0.6rem] lg:block lg:h-[4.5rem]"
+            />
+          </div>
+          {/* email */}
+          <p className="py-[1rem] pb-[3rem] font-bruno text-[1.5rem] font-normal md:text-[2rem] lg:pb-[4rem] lg:text-[2rem]">
+            fullyhacks@gmail.com
+          </p>
+
+          {/* Frame for icons */}
+          <div className="flex flex-grow items-center justify-center gap-6 text-[#ECECEC]">
             <a
               target="_blank"
               href="https://www.linkedin.com/company/fullyhacks/">
-              <BsLinkedin className="h-12 w-12 md:h-16 md:w-16" />
+              <BsLinkedin className="h-[2.75rem] w-12 md:h-14 md:w-14" />
             </a>
             <a target="_blank" href="https://discord.gg/3NvZKuQxJY">
-              <BsDiscord className="h-12 w-12 md:h-16 md:w-16" />
+              <BsDiscord className="h-[3rem] w-12 md:h-16 md:w-16" />
             </a>
-            <a target="_blank" href="https://acmcsuf.com/">
-              <img
-                className="h-12 w-12 md:h-16 md:w-16"
-                src="/assets/acm_logo.svg"
-                alt="ACM Logo"
-              />
-            </a>
+            <div className="relative inline-block">
+              {/* ACM text */}
+              <a target="_blank" href="https://acmcsuf.com/">
+                <img
+                  src="/assets/footer/capyACM.svg"
+                  alt="capybara ACM"
+                  className="absolute hidden h-[3rem] w-auto lg:top-[-1.5rem] lg:left-[-0.7rem] lg:block"
+                />
+                <img
+                  className="md:h-18 md:w-18 h-14 w-14"
+                  src="/assets/acm_logo.svg"
+                  alt="ACM Logo"
+                />
+              </a>
+            </div>
             <a target="_blank" href="https://www.instagram.com/fullyhacks/">
-              <BsInstagram className="h-8 w-8 md:h-12 md:w-12" />
+              {<PiInstagramLogoFill className="h-12 w-12 md:h-16 md:w-16" />}
             </a>
           </div>
         </div>
+      </div>
+      {/* Footer Text */}
+      <div className="bottom-0 w-full pt-10 text-center lg:hidden">
+        <p className="text-sm text-gray-400 lg:hidden">
+          © 2025 FullyHacks. All Rights Reserved.
+        </p>
       </div>
     </>
   );
