bcca127203818bd9020457a7a9296068a1c26039
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2025-01-09 13:22:50 -0800

    add application closing date to /apply

--- src/app/(site)/apply/_components/apply-auth.tsx
+++ src/app/(site)/apply/_components/apply-auth.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 
 interface ApplyAuthProps {
   children?: React.ReactNode;
@@ -8,50 +8,39 @@ interface ApplyAuthProps {
 
 const ApplyAuth: React.FC<ApplyAuthProps> = ({ children }) => {
   // 2025 application opens from Feb 14th to Mar 24th
-  // App extension ends on Mar 31st
   const openDateTs = "2025-02-14T00:00:00.000-07:00";
-  const closeDateTs = "2025-03-24T23:59:59.999-07:00";
+  const applicationCloseDateTs = "2025-03-24T23:59:59.999-07:00";
+  const closeDateTs = "2025-04-13T23:59:59.999-07:00";
+
   const options = { timeZone: "America/Los_Angeles" };
 
   const openDate = new Date(openDateTs).toLocaleString("en-US", options);
   const closeDate = new Date(closeDateTs).toLocaleString("en-US", options);
-  const now = new Date().toLocaleString("en-US", options);
+  const applicationCloseDate = new Date(
+    applicationCloseDateTs
+  ).toLocaleDateString("en-US", options);
 
-  const [applicationOpen, setOpen] = useState<boolean>(() => {
-    const currentDate = new Date();
-    const opening = new Date(openDate);
-    const closing = new Date(closeDate);
-    return currentDate >= opening && currentDate <= closing;
-  });
+  const now = new Date().toLocaleString("en-US", options);
+  const applicationOpen =
+    new Date(now) >= new Date(openDate) &&
+    new Date(now) <= new Date(applicationCloseDate);
+
+  if (applicationOpen) {
+    return <>{children}</>;
+  }
 
   return (
-    <>
-      {applicationOpen ? (
-        children
-      ) : (
-        <div className="mt-10 flex flex-col items-center justify-center text-center text-lg font-semibold text-white md:text-[2.5rem]">
-          {new Date(now) < new Date(openDate) && (
-            <div className="flex flex-col items-center gap-12 text-center">
-              <p className="w-[80%]">
-                Application will open on Feb 14th, see you soon!.
-              </p>
-              {/* TODO: update image here? */}
-              {/* <img */}
-              {/*   src="/assets/byecat.svg" */}
-              {/*   alt="bye cat" */}
-              {/*   className="w-[300px]" */}
-              {/* /> */}
-            </div>
-          )}
-          {new Date(now) >= new Date(closeDate) && (
-            <p>
-              Fullyhacks {new Date(closeDate).getFullYear()} has ended,
-              we&apos;ll see you next year 🐘.
-            </p>
-          )}
+    <div className="mt-10 flex flex-col items-center justify-center text-center text-lg font-semibold text-white md:text-[2.5rem]">
+      {new Date(now) < new Date(openDate) ? (
+        <div className="flex flex-col items-center gap-12 text-center">
+          <p>Applications will open on Feb 14th, see you soon!</p>
         </div>
+      ) : new Date(now) >= new Date(closeDate) ? (
+        <p>Fullyhacks 2025 has ended, we&apos;ll see you next year 🐘.</p>
+      ) : (
+        <p>Applications for FullyHacks 2025 have closed.</p>
       )}
-    </>
+    </div>
   );
 };
 
--- src/app/(site)/apply/page.tsx
+++ src/app/(site)/apply/page.tsx
@@ -26,86 +26,37 @@ export default async function ApplyPage() {
   const { user, application } = await getApplicationData(session);
 
   return (
-    <>
-      <section>
-        <Flower
-          width="w-24 md:w-32"
-          height="h-24 md:w-32"
-          left="left-5"
-          top="top-[12rem]"
-          noMotion={true}
-          flowerType={1}
-        />
-
-        <Flower
-          width="w-36 md:w-52"
-          height="w-36 md:h-52"
-          left="left-8 md:left-10"
-          top="top-[30rem] md:top-[20rem]"
-          noMotion={true}
-          flowerType={3}
-        />
-
-        <Flower
-          width="w-16 md:w-24"
-          height="w-16 md:h-24"
-          left="left-6 md:left-12"
-          top="top-[32rem] md:top-[28rem]"
-          noMotion={true}
-          flowerType={3}
-        />
-
-        <Flower
-          width="w-16 md:w-24"
-          height="w-16 md:h-24"
-          left=""
-          right="right-[2rem] md:right-[4rem]"
-          top="top-[7rem]"
-          noMotion={true}
-          flowerType={2}
-        />
-
-        <Flower
-          width="w-28 md:w-52"
-          height="w-28 md:h-52"
-          left=""
-          right="right-[3rem] md:right-[6rem]"
-          top="top-[12rem]"
-          noMotion={true}
-          flowerType={4}
-        />
-
-        <AuthNavBar />
-        {application?.applied ? (
-          <>
-            <div className="mt-10 flex flex-col items-center justify-center text-center text-purple_main">
-              <p className="text-lg font-semibold md:text-xl">
-                You&apos;ve already submitted an application
+    <section>
+      <AuthNavBar />
+      {application?.applied ? (
+        <>
+          <div className="mt-10 flex flex-col items-center justify-center text-center text-purple_main">
+            <p className="text-lg font-semibold md:text-xl">
+              You&apos;ve already submitted an application
+            </p>
+            <Link href="/portal" className="purple-btn mt-10">
+              Go to User Portal
+            </Link>
+          </div>
+        </>
+      ) : (
+        <ApplyAuth>
+          <div className="flex items-center justify-center">
+            <div className="z-10 mt-10 mb-32 flex w-[min(50rem,_90vw)] flex-col items-center rounded-lg border-8 border-blue_border_rgba bg-blue_rgba p-8 text-[#FF35EB] [box-shadow:_0_0_32px_#618AA8]">
+              <p className="text-lg font-semibold text-[#FF35EB] md:text-[3rem]">
+                MY APPLICATION
               </p>
-              <Link href="/portal" className="purple-btn mt-10">
-                Go to User Portal
-              </Link>
+              <p className="font-light text-white md:text-[1.5rem]">
+                Draft will be saved
+              </p>
+              <p className="mt-4 text-[1.25rem] font-semibold text-white">
+                *Application due by Saturday, February 10th*
+              </p>
+              <ApplicationForm url={user?.image} />
             </div>
-          </>
-        ) : (
-          <ApplyAuth>
-            <div className="flex items-center justify-center">
-              <div className="z-10 mt-10 mb-32 flex w-[min(50rem,_90vw)] flex-col items-center rounded-lg border-8 border-blue_border_rgba bg-blue_rgba p-8 text-[#FF35EB] [box-shadow:_0_0_32px_#618AA8]">
-                <p className="text-lg font-semibold text-[#FF35EB] md:text-[3rem]">
-                  MY APPLICATION
-                </p>
-                <p className="font-light text-white md:text-[1.5rem]">
-                  Draft will be saved
-                </p>
-                <p className="mt-4 text-[1.25rem] font-semibold text-white">
-                  *Application due by Saturday, February 10th*
-                </p>
-                <ApplicationForm url={user?.image} />
-              </div>
-            </div>
-          </ApplyAuth>
-        )}
-      </section>
-    </>
+          </div>
+        </ApplyAuth>
+      )}
+    </section>
   );
 }
