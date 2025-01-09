1a26accd9b38a1cdc2909cf0c39fe7547ce30f82
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-01-09 13:27:17 -0800

    update apply-auth for 2025 (#93)
    * update apply-auth for 2025
    
    * add application closing date to /apply
    
    ---------
    
    Co-authored-by: Chom <112128328+tomasohCHOM@users.noreply.github.com>

--- src/app/(site)/apply/_components/apply-auth.tsx
+++ src/app/(site)/apply/_components/apply-auth.tsx
@@ -1,52 +1,46 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
+
 interface ApplyAuthProps {
   children?: React.ReactNode;
 }
+
 const ApplyAuth: React.FC<ApplyAuthProps> = ({ children }) => {
-  // Application from March 13rd to April 1st
+  // 2025 application opens from Feb 14th to Mar 24th
+  const openDateTs = "2025-02-14T00:00:00.000-07:00";
+  const applicationCloseDateTs = "2025-03-24T23:59:59.999-07:00";
+  const closeDateTs = "2025-04-13T23:59:59.999-07:00";
+
   const options = { timeZone: "America/Los_Angeles" };
-  const openDate = new Date("2024-01-14T00:00:00.000-07:00").toLocaleString(
-    "en-US",
-    options
-  );
-  const closeDate = new Date("2024-02-13T23:59:59.999-07:00").toLocaleString(
-    "en-US",
-    options
-  );
+
+  const openDate = new Date(openDateTs).toLocaleString("en-US", options);
+  const closeDate = new Date(closeDateTs).toLocaleString("en-US", options);
+  const applicationCloseDate = new Date(
+    applicationCloseDateTs
+  ).toLocaleDateString("en-US", options);
+
   const now = new Date().toLocaleString("en-US", options);
-  const [applicationOpen, setOpen] = useState<boolean>(() => {
-    const currentDate = new Date();
-    const opening = new Date(openDate);
-    const closing = new Date(closeDate);
-    return currentDate >= opening && currentDate <= closing;
-  });
+  const applicationOpen =
+    new Date(now) >= new Date(openDate) &&
+    new Date(now) <= new Date(applicationCloseDate);
+
+  if (applicationOpen) {
+    return <>{children}</>;
+  }
 
   return (
-    <>
-      {applicationOpen ? (
-        children
-      ) : (
-        <div className="mt-10 flex flex-col items-center justify-center text-center text-lg font-semibold text-white md:text-[2.5rem]">
-          {new Date(now) < new Date(openDate) && (
-            <div className="flex flex-col items-center gap-12 text-center">
-              <p className="w-[80%]">
-                Application will open on January 12th, see you soon!.
-              </p>
-              <img
-                src="/assets/byecat.svg"
-                alt="bye cat"
-                className="w-[300px]"
-              />
-            </div>
-          )}
-          {new Date(now) >= new Date(closeDate) && (
-            <p>Fullyhacks 2024 has ended, we&apos;ll see you next year 🐘.</p>
-          )}
+    <div className="mt-10 flex flex-col items-center justify-center text-center text-lg font-semibold text-white md:text-[2.5rem]">
+      {new Date(now) < new Date(openDate) ? (
+        <div className="flex flex-col items-center gap-12 text-center">
+          <p>Applications will open on Feb 14th, see you soon!</p>
         </div>
+      ) : new Date(now) >= new Date(closeDate) ? (
+        <p>Fullyhacks 2025 has ended, we&apos;ll see you next year 🐘.</p>
+      ) : (
+        <p>Applications for FullyHacks 2025 have closed.</p>
       )}
-    </>
+    </div>
   );
 };
 
