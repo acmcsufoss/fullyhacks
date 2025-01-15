bdb6347e9ddeb398228ff04992bd9cf821b7bd8f
Jeremiah <jeremherring@gmail.com>
2025-01-14 17:04:00 -0800

    add changes to tracks and prizes

--- src/app/(site)/feed/prizes/page.tsx
+++ src/app/(site)/feed/prizes/page.tsx
@@ -38,45 +38,28 @@ const tracks: tracksType[] = [
 
 function Card({ track }: { track: tracksType }) {
   return (
-    <div
-      className="flex h-[494px] w-[505px] cursor-pointer flex-col items-center rounded-[61.64px] bg-[#173162] p-6 shadow-xl duration-200 ease-in-out hover:bg-[rgb(52,11,103)] hover:text-white"
-    >
-      {/* Top Section with Track Name and SVGs */}
-      <div className="relative flex items-center justify-center w-full h-[61px] mt-6">
-        {/* Left SVG */}
+    <div className="flex h-[494px] w-full max-w-[480px] cursor-pointer flex-col items-center rounded-[61.64px] bg-[#173162] p-6 shadow-xl duration-200 ease-in-out hover:bg-[rgb(52,11,103)] hover:text-white">
+      <div className="relative mt-6 flex h-[61px] w-full items-center justify-center">
         <div className="absolute left-0 flex h-[61px] w-[61px]">
-          <img
-          src="/assets/circles.svg"
-          alt="circles"
-          ></img>
+          <img src="/assets/circles.svg" alt="circles" />
         </div>
 
-        {/* Track Name */}
         <p className="w-[355px] text-center text-lg font-bold text-white">
           {track.name}
         </p>
 
-        {/* Right SVG */}
         <div className="absolute right-0 flex h-[61px] w-[61px]">
-          <img
-          src="/assets/circles.svg"
-          alt="circles"
-          >
-          </img>
+          <img src="/assets/circles.svg" alt="circles" />
         </div>
       </div>
 
-      {/* Track Description */}
       <div className="mt-6 flex h-[180px] w-[273px] flex-col items-center text-center">
         {track.team && (
           <p className="mb-4 font-normal leading-7 text-white">
             Total: {track.team} team(s)
           </p>
         )}
-        <p
-          className="font-audiowide text-[20px] font-[400] leading-[30px] text-white"
-          style={{ width: "273px", lineHeight: "30px", height: "180px" }}
-        >
+        <p className="font-audiowide text-[20px] font-[400] leading-[30px] text-white">
           {track.description}
         </p>
       </div>
@@ -86,22 +69,27 @@ function Card({ track }: { track: tracksType }) {
 
 export default function Prizes() {
   return (
-    <section className="my-14 mr-8 w-full text-white">
-      <p className="feed-title">Tracks & Prizes</p>
-      <p className="md:text-md">
+    <section className="my-14 mr-8 w-full px-4 text-white lg:px-8">
+      <p className="h-[95px] w-[565px] font-audiowide text-[64px] font-[400] leading-[81.59px] text-[#72D6E6]">
+        Tracks & Prizes
+      </p>
+      <p
+        className="h-[31px] w-[732px] font-audiowide text-[24px] font-[400] leading-[30.6px] text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+        style={{ letterSpacing: "5%" }}>
         Find all details about tracks, prize categories here
       </p>
+
       <div className="mt-10">
         <div className="mb-4 flex items-center gap-4">
-          <p className="text-lg font-bold leading-normal text-purple-500">
+          <p className="font-rubik text-[27px] font-[700] leading-[20px] text-[#72D6E6]">
             TRACKS
           </p>
         </div>
-        <p className="font-normal text-white md:text-md">
+        <p className="font-audiowide text-white md:text-md">
           Hackers have to build their projects around one of these 4 tracks
         </p>
       </div>
-      <div className="mt-10 grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-3">
+      <div className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
         {tracks.map((track: tracksType) => {
           return <Card key={track.id} track={track} />;
         })}
@@ -122,7 +110,7 @@ export default function Prizes() {
         <p className="text-lg font-bold leading-normal text-purple-500">
           MAIN CATEGORY
         </p>
-        <div className="mt-10 grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
           {mainCategory.map((category: tracksType) => {
             return <Card key={category.id} track={category} />;
           })}
@@ -135,7 +123,7 @@ export default function Prizes() {
         <p className="font-normal leading-normal md:text-md">
           You can choose ONE sub category along with your project
         </p>
-        <div className="mt-10 grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
           {subCategory.map((category: tracksType) => {
             return <Card key={category.id} track={category} />;
           })}
