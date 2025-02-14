a0abc447fbd26da7cca74e3233b9c0a363f5ae8b
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2025-02-13 20:05:34 -0800

    chore: open apps at 12 PM

--- src/app/(site)/apply/_components/apply-auth.tsx
+++ src/app/(site)/apply/_components/apply-auth.tsx
@@ -7,7 +7,7 @@ interface ApplyAuthProps {
 }
 
 const ApplyAuth: React.FC<ApplyAuthProps> = ({ children }) => {
-  const openDateTs = "2025-02-14T00:00:00.000-07:00";
+  const openDateTs = "2025-02-14T12:00:00.000-08:00";
   const applicationCloseDateTs = "2025-03-24T23:59:59.999-07:00";
   const closeDateTs = "2025-04-13T23:59:59.999-07:00";
 
