caab2b1bf3b8cbfe5bd51a2e25fdbe22df8e39b1
Justin Stitt <jstitt007@gmail.com>
2025-03-17 19:34:28 -0700

    fix: remove statement regarding application dates (#128)
    Application dates have been revealed. Applications are due "...by 11:59 PM
    (PDT) on March 24, 2025."
    
    Try to avoid misleading readers of the FAQ into believing the
    applications are not open.
    
    Thankfully, it is likely that users have been misled:
    
    1) The apply button is active and works
    
    2) Other FAQ question(s) mentions due dates for applications
    
    ---
    So, unless I've misunderstood this sentence, we should fix up the faq :>)

--- src/lib/data/faq.ts
+++ src/lib/data/faq.ts
@@ -11,7 +11,7 @@ export const faqs: FAQType[] = [
     id: "faq2",
     question: "When is FullyHacks?",
     answer:
-      "FullyHacks will take place from April 12th to April 13th. The hackathon will be 100% in-person. Application dates will be revealed very soon :)"
+      "FullyHacks will take place from April 12th to April 13th. The hackathon will be 100% in-person."
   },
   {
     id: "faq3",
