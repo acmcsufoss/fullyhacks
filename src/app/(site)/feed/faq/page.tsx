be29be15656e0513e781af4402f937a3805ed25e
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2025-03-26 20:25:49 -0700

    modify feed faqs

--- src/app/(site)/feed/faq/page.tsx
+++ src/app/(site)/feed/faq/page.tsx
@@ -9,84 +9,85 @@ const faqs: FAQType[] = [
     id: "faq0",
     question: "When should I arrive?",
     answer:
-      "The check-in will start from 1pm to 3pm on Feb 24th in Mihaylo room 1502."
-  },
-  {
-    id: "faq1/2",
-    question: "When will the hackathon start?",
-    answer:
-      "The hackathon will officially start at 4pm Feb 24th. After the opening ceremony, we'll go back to CS buildings"
-  },
-  {
-    id: "faq01",
-    question: "When will the hackathon end?",
-    answer:
-      "The hackathon ends at 4pm Feb 25th, closing ceremony will be at 6pm."
-  },
-  {
-    id: "faq17",
-    question: "Can I leave during the hackathon?",
-    answer:
-      "Yes, you can leave then come back, make sure to check-in when you come back"
-  },
-  {
-    id: "faq18",
-    question: "Where is the information room?",
-    answer:
-      "Information room is in CS110A, you can come to check-in or ask questions"
-  },
-  {
-    id: "faq02",
-    question: "Where do I park my vehicles?",
-    answer:
-      "There is no charge for parking at CSUF on Saturday and Sunday, we'd recommend Lot A, Eastside for overnight parking."
-  },
-  {
-    id: "faq03",
-    question: "Will there be food?",
-    answer:
-      "We will have Pizza the first day for dinner and some Pastries and Fruit for breakfast Sunday. Snacks and drinks will be provided and be free to grab during the whole event. "
+      "Check-in will be from 1 PM to 3 PM on April 12th in Mihaylo Room 1502."
   },
   {
     id: "faq1",
-    question: "Does FullyHacks have just one theme or multiple themes?",
+    question: "When will the hackathon start?",
     answer:
-      "FullyHacks has multiple themes that are divided into four different tracks."
+      "The hackathon will officially start at 4 PM on April 12th. After the opening ceremony, we'll head back to the CS buildings."
+  },
+  {
+    id: "faq2",
+    question: "When will the hackathon end?",
+    answer:
+      "The hackathon ends at 4 PM on April 13th. The closing ceremony will be at 6 PM."
+  },
+  {
+    id: "faq3",
+    question: "Can I leave during the hackathon?",
+    answer:
+      "Yes, you can leave and return. Just make sure to check in when you come back."
+  },
+  {
+    id: "faq4",
+    question: "Where is the information room?",
+    answer:
+      "The information room is in CS110A. You can come here to check in or ask questions."
+  },
+  {
+    id: "faq5",
+    question: "Where do I park my vehicle?",
+    answer:
+      "Parking at CSUF is free on Saturday and Sunday. We recommend Lot A, Eastside, for overnight parking."
+  },
+  {
+    id: "faq6",
+    question: "Will there be food?",
+    answer:
+      "Yes! We will have pizza on the first day for dinner and pastries and fruit for breakfast on Sunday. Snacks and drinks will be available throughout the event."
+  },
+  {
+    id: "faq7",
+    question: "Does FullyHacks have one theme or multiple themes?",
+    answer:
+      "FullyHacks has multiple themes, divided into four different tracks."
+  },
+  {
+    id: "faq8",
+    question: "What should I bring?",
+    answer:
+      "A sleeping bag, laptop, chargers, snacks, drinks, hygiene items, and anything else you may need."
+  },
+  {
+    id: "faq9",
+    question: "How do I prepare for the hackathon?",
+    answer:
+      "If you have no coding experience, check out our FullyPacks template. Make sure your coding environment is set up in advance, including version control (GitHub), an IDE (VSCode), and any necessary frameworks or runtimes (Python, Node.js, etc.)."
+  },
+  {
+    id: "faq10",
+    question: "What are FullyPacks? Do I have to use them?",
+    answer:
+      "No, you're not required to use them. FullyPacks are beginner-friendly templates designed to help you start your project if you don't know where to begin."
   },
   {
     id: "faq11",
-    question: "What should I bring?",
-    answer: "Sleeping bag, laptop, chargers, snacks & drinks, hygiene items,..."
+    question: "How do teams work? What if I don't have a team?",
+    answer:
+      "Teams can have 1 to 4 members. If you don't have a team, we will host a team-forming event after the opening ceremony."
   },
   {
     id: "faq12",
-    question: "How do I prepare for the hackathon?",
+    question: "What if I have questions during the hackathon?",
     answer:
-      "It would be a good idea to explore our Fullypacks template if you have no coding experience and have your coding enviroment setup beforehand, such as having version control (Github), an IDE (VSCode) and whatever necessary frameworks or runtimes your project needs (python, node, etc)."
+      "Please reach out to any of the mentors in your room or ask in our Discord forum channel."
   },
   {
     id: "faq13",
-    question: "What are Fullypacks? Do I have to use them?",
-    answer:
-      "No, you're not required to use one to build your project. Fullypacks are our beginner-friendly templates to help you kickstart your project if you don't know where to start."
-  },
-  {
-    id: "faq14",
-    question: "How do teams work? What if I dont have a team?",
-    answer:
-      "Teams are size of 1-4. If you would like team up with others, we will have a team forming event after opening ceremony"
-  },
-  {
-    id: "faq15",
-    question: "What if I have questions during the hackathon?",
-    answer:
-      "Please reach out to any of the mentors in your room for questions or ask in our Discord forum channel."
-  },
-  {
-    id: "faq16",
     question: "How will the projects be judged?",
     answer:
-      'There are different prize categories that will be announced the day of the hackathon that you can sign up your project during the team forming event, for example "Best use of..."'
+      "There will be different prize categories announced on the day of the hackathon. You can sign up your project for a category during the team-forming event. Examples include 'Best Use of...' awards."
   }
 ];
 
