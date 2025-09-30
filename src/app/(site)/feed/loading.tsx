e795f546698414555296612866e4996d7bdc80eb
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-09-30 14:09:52 -0700

    fix: remove semicolon in feed loading spinner

--- src/app/(site)/feed/loading.tsx
+++ src/app/(site)/feed/loading.tsx
@@ -3,7 +3,7 @@ import { AiOutlineLoading } from "react-icons/ai";
 export default function Loading() {
   return (
     <div className="mt-4 flex items-center justify-center">
-      <AiOutlineLoading size={48} className="animate-spin text-[#72d6e6]" />;
+      <AiOutlineLoading size={48} className="animate-spin text-[#72d6e6]" />
     </div>
   );
 }
